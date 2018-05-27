a8ba2b559e3d5e16267e151ecf81d568022cdf43
alexen2 <psany@yandex.ru>
2018-05-27 11:31:52 +0300

    fix

--- src/blocks/form-request-demo/form-request-demo.js
+++ src/blocks/form-request-demo/form-request-demo.js
@@ -29,6 +29,17 @@
 		  	err = false;
 		  	break;
 		  }
+
+		  if(element[i] == 'email') {
+		  	var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		  	var address = document.getElementById(element[i]);
+				
+		  	if(!address.value.match(reg)) {
+		  		address.parentNode.classList.add("input-anim--error");
+		  		err = false;
+		  		break;
+		  	}
+		  }
 		}
 
 		if(err) formRequestDemoForm.submit();
